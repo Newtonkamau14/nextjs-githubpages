478840adab21e6c54fc16042a35255f1e3b5b965
Newtonkamau14 <newtonkamau14@gmail.com>
2025-03-05 21:19:57 +0300

    feat: update next.config.ts and install github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig:NextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/nextjs-githubpages/' : '',
+  basePath: isProd ? '/nextjs-githubpages' : '',
+  output: 'export'
 };
 
 export default nextConfig;
